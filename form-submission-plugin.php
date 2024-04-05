068b7a77517b8323365c9dbb9ec7cbeb7101595f
emilyr1226 <104455819+emilyr1226@users.noreply.github.com>
2024-04-04 22:32:30 -0400

    update record

--- form-submission-plugin.php
+++ form-submission-plugin.php
@@ -117,17 +117,65 @@ function form_submission() {
                     )
                 );
                 break;
-            case 'record':
+           case 'record':
                 // Handle record form data insertion
-                $species = sanitize_text_field($_POST['record_species']);
-                $name = sanitize_text_field($_POST['record_name']);
+                $species = sanitize_text_field($_POST['record-species']);
+                $name = sanitize_text_field($_POST['record-name']);
+                $received_date = sanitize_text_field($_POST['record-received-date']);
+                $link = sanitize_text_field($_POST['record-link']);
+                $selected_type = sanitize_text_field($_POST['record_type']);
+                 // Only shows up when type is wild
+                if ($selected_type === 'wild' && isset($_POST['record-provider'])) {
+                    $provider = sanitize_text_field($_POST['record-provider']);
+                    $lat = sanitize_text_field($_POST['record-lat']);
+                    $long = sanitize_text_field($_POST['record-long']);
+                    $accuracy = sanitize_text_field($_POST['record-accuracy']);
+                    $locality = sanitize_text_field($_POST['record-locality']);
+                    $location = sanitize_text_field($_POST['record-location']);
+                    $stream = sanitize_text_field($_POST['record-stream']);
+                    $host_species = sanitize_text_field($_POST['record-host-species']);
+                    $host_descr = sanitize_text_field($_POST['record-host-descr']);
+                }
+                $selected_nuclear = sanitize_text_field($_POST['nuclear']);
+                $allele1 = sanitize_text_field($_POST['allele-1']);
+                if ($selected_nuclear == 'dikaryon' && isset($_POST['record-allele-2'])) {
+                    $allele2 = sanitize_text_field($_POST['record-allele-2']);
+                }
+                if ($selected_type != 'wild' && isset($_POST['record-parent-1'])) {
+                    $parent1 = sanitize_text_field($_POST['record-parent-1']);
+                }
+                if ($selected_type === 'cross' || $selected_type === 'test-cross' && isset($_POST['record-parent-2'])) {
+                    $parent2 = sanitize_text_field($_POST['record-parent-2']);
+                }
+                $notes = sanitize_text_field($_POST['record-notes']);
+                $projects = sanitize_text_field($_POST['record-projects']);
+
+            
                 // Implement insertion logic for records table
                 $wpdb->insert(
                     'records',
                     array(
                         'species' => $species,
                         'name' => $name,
-                        // Add more fields and values as needed
+                        'received_date' => $received_date,
+                        'link' => $link,
+                        'type' => $selected_type, 
+                        'provider' => $provider,
+                        'lat' => $lat,
+                        'long' => $long,
+                        'accuracy' => $accuracy,
+                        'locality' => $locality,
+                        'location' => $location,
+                        'stream' => $stream, 
+                        'host_species' => $host_species,
+                        'host_descr' => $host_descr,
+                        'selected_nuclear' => $selected_nuclear,
+                        'allele1' => $allele1,
+                        'allele2' => $allele2,
+                        'parent1' => $parent1,
+                        'parent2' => $parent2,
+                        'notes' => $notes, 
+                        'projects' => $projects,
                     )
                 );
                 break;
